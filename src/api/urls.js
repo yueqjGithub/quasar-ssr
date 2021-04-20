24af864c8e50575abd1b7d08178e61708c8af6df
yueqjGit <410281036@qq.com>
2021-04-20 18:27:02 +0800

    'fixhttps'

--- src/api/urls.js
+++ src/api/urls.js
@@ -1,11 +1,11 @@
 // const mockUrl = 'https://www.fastmock.site/mock/c4a9191b46d02bfc455e710d4f53419e/api/gameDateManage'
-let baseUrl = 'http://wwwadminapi.agdgame.cn'
+let baseUrl = 'https://wwwadminapi.agdgame.cn'
 const downloadPage = 'https://test.sdk.chaoyi.cn'
 // let baseUrl = '/'
 // const baseUrl = '/gameDateManage'
 
 if (process.env.CUS_ENV === 'PROD_ENV') {
-  baseUrl = 'http://wwwadminapi.agdgame.cn'
+  baseUrl = 'https://wwwadminapi.agdgame.cn'
 }
 
 const urls = {
