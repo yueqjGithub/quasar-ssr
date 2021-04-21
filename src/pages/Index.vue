1658fe26af1c72cc04115fbe3995523271885001
yueqjGit <410281036@qq.com>
2021-04-21 15:32:20 +0800

    'fix'

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -22,8 +22,8 @@
         <div class="full-width flex-row flex-jst-btw flex-ali-center">
           <div class="self-stretch flex-col flex-jst-start flex-ali-center act-module act-left">
             <div class="full-width flex-row flex-jst-start flex-ali-start">
-              <img src="~/assets/img/1_41.png" alt="" class="act-img">
-              <p class="font-16 font-bold text-black">龙城霸业</p>
+              <img src="~/assets/img/1_41.png?v=2" alt="" class="act-img">
+              <p class="font-16 font-bold text-black">龙城凯歌</p>
             </div>
             <div class="self-stretch flex-row flex-jst-start flex-ali-start act-left-cont">
               <p class="p-col">
@@ -73,7 +73,7 @@ export default {
     return {
       info: null,
       activeList: [
-        { img: actImg1, title: '至尊传奇', description: '全平台上线，狂刷装备' },
+        { img: actImg1, title: '仙道独尊', description: '全平台上线，狂刷装备' },
         { img: actImg2, title: '女武神降临', description: '全平台上线，狂刷装备' }
       ]
     }
